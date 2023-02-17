9e6c028c2e75889c1e31be24061c07d05345436d
Sindhuja Jishnu <sp1557@student.aru.ac.uk>
2023-02-17 10:30:32 +0000

    2nd commit

--- Eazy_shop/admin.py
+++ Eazy_shop/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import category
+from .models import Product
 
 # Register your models here.
+admin.site.register(category)
+admin.site.register(Product)
