3797f019c2941c0d43c068e8378b69b8a6dca236
Sindhuja Jishnu <sp1557@student.aru.ac.uk>
2023-04-21 09:02:58 +0100

    final commit

--- Eazy_shop/apps.py
+++ Eazy_shop/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EazyShopConfig(AppConfig):
+class ShopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Eazy_shop'
+    name = 'shop'
