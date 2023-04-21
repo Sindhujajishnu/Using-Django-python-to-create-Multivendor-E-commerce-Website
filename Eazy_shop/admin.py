19dac6a291f0f9c8d854bd45870b87dda23a8466
Sindhuja Jishnu <sp1557@student.aru.ac.uk>
2023-04-21 09:03:30 +0100

    final commit

--- Eazy_shop/admin.py
+++ Eazy_shop/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import category
-from .models import Product
-
+from .models import *
 # Register your models here.
-admin.site.register(category)
-admin.site.register(Product)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ('name','description','status')
+admin.site.register(Catagory,CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ('name', 'vendor','original_price','selling_price', 'description')
+admin.site.register(Product,ProductAdmin)
+
+
+class VendorAdmin(admin.ModelAdmin):
+  list_display = ('name', 'address','contact','status')
+admin.site.register(Vendor,VendorAdmin)
+
