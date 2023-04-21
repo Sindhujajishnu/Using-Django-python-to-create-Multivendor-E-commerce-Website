661f9de7db3a7a18c6438e4449429e3541ae0e18
Sindhuja Jishnu <sp1557@student.aru.ac.uk>
2023-04-21 09:02:28 +0100

    final commit

--- Eazy_shop/models.py
+++ Eazy_shop/models.py
@@ -1,35 +1,80 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 import datetime
 import os
 
-def getFileName(request,filename):
-    now_time=datetime.datetime.now().strftime("%Y%m%d%H:%M:%S")
-    new_filename="%s%s"%(now_time,filename)
-    return os.path.join('uploads/',new_filename)
 
-class category(models.Model):
-    name=models.CharField(max_length=150,null=False,blank=False)
-    image=models.ImageField(upload_to=getFileName,null=True,blank=True)
-    description=models.TextField(max_length=500,null=False,blank=False)
-    status=models.BooleanField(default=False,help_text="0-show,1-Hidden")
-    created_at=models.DateTimeField(auto_now_add=True)
+def getFileName(requset, filename):
+    now_time = datetime.datetime.now().strftime("%Y%m%d%H:%M:%S")
+    new_filename = "%s%s" % (now_time, filename)
+    print(os.path)
+    return os.path.join('uploads/', new_filename)
 
-def __str__(self):
- return  self.name
+
+class Catagory(models.Model):
+    name = models.CharField(max_length=150, null=False, blank=False)
+    image = models.ImageField(upload_to=getFileName, null=True, blank=True)
+    description = models.TextField(max_length=500, null=False, blank=False)
+    status = models.BooleanField(default=True, help_text="0-show,1-Hidden")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=150, null=False, blank=False)
+    address = models.TextField(max_length=500, null=False, blank=False)
+    contact = models.CharField(max_length=150, null=False, blank=False)
+    email = models.CharField(max_length=150, null=False, blank=False)
+    status = models.BooleanField(default=True, help_text="0-active,1-not_active")
+    register_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
-    category=models.ForeignKey(category,on_delete=models.CASCADE)
-    name=models.CharField(max_length=150,null=False,blank=False)
-    vendor=models.CharField(max_length=150,null=False,blank=False)
-    product_image=models.ImageField(upload_to=getFileName,null=True,blank=True)
-    quantity=models.IntegerField(null=False,blank=False)
-    original_price=models.FloatField(null=False,blank=False)
-    selling_price=models.FloatField(null=False,blank=False)
-    description=models.TextField(max_length=500,null=False,blank=False)
-    status=models.BooleanField(default=False,help_text="0-show,1-Hidden")
-    trending=models.BooleanField(default=False,help_text="0-default,1-trending")
-    created_at=models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Catagory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150, null=False, blank=False)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    product_image = models.ImageField(upload_to=getFileName, null=True, blank=True)
+    quantity = models.IntegerField(null=False, blank=False)
+    original_price = models.FloatField(null=False, blank=False)
+    selling_price = models.FloatField(null=False, blank=False)
+    description = models.TextField(max_length=500, null=False, blank=False)
+    status = models.BooleanField(default=True, help_text="0-show,1-Hidden")
+    trending = models.BooleanField(default=False, help_text="0-default,1-Trending")
+    created_at = models.DateTimeField(auto_now_add=True)
 
-def __str__(self):
- return self.name 
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_qty = models.IntegerField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def total_cost(self):
+        return self.product_qty * self.product.selling_price
+
+
+class Favourite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ChatMessage(models.Model):
+    body = models.TextField()
+    msg_sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="msg_sender")
+    msg_receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="msg_receiver")
+    seen = models.BooleanField(default=False)
+    message_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
